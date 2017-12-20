905227297c0b75c94c061485a26ccf5dbd6eaede
Lian <lian@connectivityAir.local>
2017-12-20 15:41:28 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A project of my d3js practice.
---
+++ graph-list.php
@@ -0,0 +1,9 @@
+<?php
+$type = htmlspecialchars($_GET["type"]);
+$flist = scandir("json/");
+foreach($flist as $file){
+    if(strpos($file, $type) !== false){
+        echo "<a href=\"".$type.".html?input=".$file."\">".$file."</a><br>";
+    }
+}
+?>
