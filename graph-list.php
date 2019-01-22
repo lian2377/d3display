3075eca234bc4671c9b7dd369ba45963425e5f1e
Lian <lian@connectivityAir.local>
2019-01-22 15:42:41 +0800

    fix file misplaced

--- graph-list.php
+++ graph-list.php
@@ -2,7 +2,7 @@
 $type = htmlspecialchars($_GET["type"]);
 $flist = scandir("json/");
 foreach($flist as $file){
-    if(strpos($file, $type) !== false){
+    if(strpos($file, ".".$type.".") !== false){
         echo "<a href=\"".$type.".html?input=".$file."\">".$file."</a><br>";
     }
 }
